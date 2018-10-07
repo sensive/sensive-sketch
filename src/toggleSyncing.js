0c000f55e0cc86f357e14329599c095d3e2aeeea
Rolando Murillo <rolando@bandd.co>
2018-10-07 12:07:07 -0400

    clean up toggleSyncing comments

--- src/toggleSyncing.js
+++ src/toggleSyncing.js
@@ -1,7 +1,5 @@
 const Settings = require('sketch/settings')
 import { requireToken } from './setup.js'
-// import exportImage from './export-image.js'
-// import uploadImage from './upload-image.js'
 
 export function toggleSyncing(context) {
   const { document } = context
@@ -10,14 +8,6 @@ export function toggleSyncing(context) {
   const toggle = () => {
     Settings.setDocumentSettingForKey(document, 'trackDocument', !tracking)
     document.showMessage(`${tracking ? 'Stopped' : 'Started'} syncing changes for ${document.cloudName()}`)
-    
-    // if(!tracking){
-    //   document.pages().forEach((page) => {
-    //     page.artboards().forEach((artboard) => {
-    //       exportImage(document, artboard, uploadImage)
-    //     })
-    //   })
-    // }
   }
 
   requireToken(() => toggle())
