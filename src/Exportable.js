43ed1707a00a30f20a60fcdc631c3d79f0ac0b03
Rolando Murillo <rolando@bandd.co>
2019-01-03 22:41:06 -0500

    remove deprecate files

--- src/Exportable.js
+++
@@ -1,7 +0,0 @@
-export default function Exportable(document, object, format, name, scale) {
-  this.document = document
-  this.object = object
-  this.scale = scale
-  this.name = name
-  this.format = format
-}
